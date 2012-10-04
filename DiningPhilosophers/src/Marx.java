28c76677997696c79d1a3160c6cda190576724ef
Elijah Wright <elijah_wright@yahoo.com>
2012-10-03 22:56:36 -0400

    New Project

---
+++ DiningPhilosophers/src/Marx.java
@@ -0,0 +1,30 @@
+public class Marx implements Runnable {
+	// Variable to store the amount of cookies eaten.
+	int philosopher = 0;
+	String name = "";
+
+	boolean dining = true;
+
+	public void run() {
+		try {
+			// Decides to eat cookies until there is no more.
+			while (dining) {
+
+				// Check to see if another thread is eating cookies.
+
+				DiningPhilosophers philoAction = new DiningPhilosophers();
+				philoAction.DiningPhilosopherRun(3);
+
+				Thread.sleep(4000);
+
+				// System.out.println( cookie );
+
+			}
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		// When done eating cookies display how many eaten.
+		System.out.println("\nI am Eli. I ate " + " cookies");
+	}
+}
---
+++ DiningPhilosophers/src/Plato.java
@@ -0,0 +1,29 @@
+public class Plato implements Runnable {
+	// Variable to store the amount of cookies eaten.
+	int philosopher = 0;
+
+	boolean dining = true;
+
+	public void run() {
+		try {
+			// Decides to eat cookies until there is no more.
+			while (dining) {
+
+				// Check to see if another thread is eating cookies.
+
+				DiningPhilosophers philoAction = new DiningPhilosophers();
+				philoAction.DiningPhilosopherRun(4);
+
+				Thread.sleep(4000);
+
+				// System.out.println( cookie );
+
+			}
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		// When done eating cookies display how many eaten.
+		System.out.println("\nI am Eli. I ate " + " cookies");
+	}
+}
