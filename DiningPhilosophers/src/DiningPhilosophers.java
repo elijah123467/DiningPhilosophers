f19208c1923643375381a1f74a5fc712fe1704fe
Elijah Wright <elijah_wright@yahoo.com>
2012-10-28 22:36:04 -0400

    More changes lol

--- DiningPhilosophers/src/DiningPhilosophers.java
+++ DiningPhilosophers/src/DiningPhilosophers.java
@@ -11,7 +11,7 @@ public class DiningPhilosophers {
 	int HUNGRY = 1;
 	int EATING = 2;
 	int mutex = 10;
-	
+
 	int[] semaphore = new int[N];
 	int[] state = new int[N];
 	String[] nameArray = new String[N];
@@ -38,9 +38,9 @@ public class DiningPhilosophers {
 		i = inI;
 
 		if (blocked == false) {
-			
-			down(mutex);
-			
+
+			down(mutex, inI);
+
 			state[inI] = HUNGRY;
 
 			System.out.println("Semaphore:" + nameArray[inI] + " "
@@ -48,12 +48,12 @@ public class DiningPhilosophers {
 
 			test(inI);
 
-			up(mutex);
+			up(mutex, inI);
 		}
 
 		if (blocked = true) {
-			
-			down(semaphore[inI]);
+
+			down(semaphore[inI], inI);
 		}
 
 	}
@@ -62,15 +62,16 @@ public class DiningPhilosophers {
 
 		i = inI;
 
-		down(mutex);
+		down(mutex, inI);
 		state[inI] = THINKING;
 
-		System.out.println("Semaphore: " + nameArray[inI] + " "
-				+ semaphore[inI] + " Thinking " + state[inI]);
+		System.out.println(nameArray[inI] + " is putting forks down. If ("
+				+ semaphore[inI] + ") is 0 can start to eat again: " + " If ("
+				+ state[inI] + ") is 0 then thinking.");
 
 		test(LEFT);
 		test(RIGHT);
-		up(mutex);
+		up(mutex, inI);
 
 	}
 
@@ -81,7 +82,7 @@ public class DiningPhilosophers {
 		if (state[inI] == HUNGRY && state[LEFT] != EATING
 				&& state[RIGHT] != EATING) {
 			state[inI] = EATING;
-			up(semaphore[inI]);
+			up(semaphore[inI], inI);
 		}
 	}
 
@@ -112,10 +113,11 @@ public class DiningPhilosophers {
 
 		try {
 			Thread.sleep(eatRand.nextInt(40));
+			System.out.println("Philosopher " + nameArray[inI]
+					+ " is done eating. ");
 			for (int i = 0; i < nameArray.length; i++) {
-				System.out.println("Philosopher " + inI + " is done eating. " + " But " + nameArray[i]
-						+ " Has ate this many times: "
-						+ countArray[i]);
+				System.out.println(" But " + nameArray[i]
+						+ " Has ate this many times: " + countArray[i]);
 			}
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
@@ -125,35 +127,38 @@ public class DiningPhilosophers {
 
 	}
 
-	private void down(int inMutex) {
+	private void down(int inMutex, int inI) {
 
 		// Handles the semaphores critical region.
-		if ((inMutex == 10 && semaphore[i] == 0 && blockedArray[i] == false)
-				|| (blockedArray[i] == true)) {
-
+		if (((inMutex == 10) && (semaphore[inI] == 0) && (blockedArray[inI] == false))
+				|| ((blockedArray[inI] == true) && (mutex == 10))) {
+			System.out.println(nameArray[inI] + " not blocked.");
 			mutex = 20;
 
-		} else if (inMutex == 20) {
+		}
+
+		else if (inMutex == 20) {
 
 			if (mutex == 20) {
-				System.out.println(nameArray[i] + " is waiting");
+				System.out.println(nameArray[inI] + " is waiting");
 			}
 
 			blocked = true;
-			System.out.println(nameArray[i] + " is blocked " + blocked);
+			System.out.println(nameArray[inI] + " is blocked " + blocked);
 		}
 
-		if (inMutex < 5) {
+		else if (inMutex < 5) {
 			for (int i = 0; i < blockedArray.length; i++) {
-				blockedArray[i] = false;
+				blockedArray[inI] = false;
 			}
-			blockedArray[i] = true;
-			down(mutex);
-			System.out.println(nameArray[i] + " is no longer blocked ");
+			blockedArray[inI] = true;
+			System.out.println(nameArray[inI] + " is blocked ");
+			down(mutex, inI);
+			System.out.println(nameArray[inI] + " is no longer blocked ");
 		}
 	}
 
-	private void up(int inMutex) {
+	private void up(int inMutex, int inI) {
 
 		// Handles the semaphores critical region.
 		if (inMutex == 20) {
@@ -164,7 +169,7 @@ public class DiningPhilosophers {
 
 		if (inMutex < 5) {
 
-			semaphore[i] = 1;
+			semaphore[inI] = 1;
 
 		}
 	}
